7933506c3f8c1e54780a52a2110bb7f0adecce70
Joao Correa <joaoishida@gmail.com>
2024-09-23 13:01:25 -0300

    feat: category property
    Added a new property to product

--- crud-lb4/src/models/product.model.ts
+++ crud-lb4/src/models/product.model.ts
@@ -27,6 +27,12 @@ export class Product extends Entity {
   })
   description?: string;
 
+  @property({
+    type: 'string',
+    default: 'n/a',
+  })
+  category?: string;
+
   // Define well-known properties here
 
   // Indexer property to allow additional data
